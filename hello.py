d9b9c54e7b957f3f2341fb32fe38be2a79b334cd
Van Le <vle4@usf.edu>
2023-09-16 15:59:55 -0400

    add TTS api

--- hello.py
+++ hello.py
@@ -1,35 +1,69 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-app = Flask(__name__)
+#requests lib for TTS api
+import requests, json, time
 
+app = Flask(__name__)
 
 #for openai API
 import openai
 openai.api_key = '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'
 model_id = 'gpt-3.5-turbo'
 
+#key for text to speech
+apikey = "c50dcb69bamshe0261669095ecbbp1aa5f1jsnb6671c12abb7"
+
+#headers for TTS API
+headers = {'content-type': "application/json", 'x-rapidapi-host': "large-text-to-speech.p.rapidapi.com", 'x-rapidapi-key': apikey}
 
 @app.route('/functioning')
 def functioning():
-    cv = ChatGPT_conversation(selected_option)
+    text = ChatGPT_conversation(selected_option)
+    TTS(text)
     return jsonify({'status': 'Done'})
     
-
+#Helper function for openAI and TTS API
 def ChatGPT_conversation(choice):
     response =  openai.ChatCompletion.create(
         model=model_id,
         messages= [{'role':'user', 'content': f"Write a story with topic {choice}"}],
+
+        #token limits
         max_tokens = 50,
         temperature = 0.6
     )
 
-    #api output
+    #OpenAi API output. - text
     output = response["choices"][0]["message"]["content"]
-    print(output)
-
     return output
+def TTS(output):
+    #TTS API
+    response = requests.request("POST", "https://large-text-to-speech.p.rapidapi.com/tts", data=json.dumps({"text": output}), headers=headers)
+    id = json.loads(response.text)['id']
+    eta = json.loads(response.text)['eta']
+    #print(f'Waiting {eta} seconds for the job to finish...')
+    #time.sleep(eta)
+    response = requests.request("GET", "https://large-text-to-speech.p.rapidapi.com/tts", headers=headers, params={'id': id})
+    while "url" not in json.loads(response.text):
+        response = requests.request("GET", "https://large-text-to-speech.p.rapidapi.com/tts", headers=headers, params={'id': id})
+        print(f'Waiting some more...')
+        time.sleep(3)
+    
+    #get URL from the API response to download soundbytes
+    url = json.loads(response.text)['url']
 
+    #
+    response = requests.request("GET", url)
+    filename = "static/audio.wav"
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    
 
+    #TTS API.
+    #print(output)
+
+    return 
+#main route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print("cool")
@@ -39,10 +73,12 @@ def index():
 
     return render_template('index.html')
 
+#after putting in names
 @app.route('/hello/<name>')
 def hello(name):
     return render_template("hello.html", name=name)
   
+
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
 
